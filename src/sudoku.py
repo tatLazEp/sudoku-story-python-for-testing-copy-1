7116f436d3ad858741e27f411ceda866d2fa7d1a
Shubham Dangarh <shubham_dangarh@epam.com>
2021-02-15 23:43:12 +0530

    Obvious Renaming

--- src/sudoku.py
+++ src/sudoku.py
@@ -1,76 +1,76 @@
-def print_sudoku(array):
+def print_sudoku(sudoku):
     print('**********************************')
-    for i in array:
+    for i in sudoku:
         for j in i:
             print(j, end=" ")
         print("")
     print('**********************************')
 
 
-def find_position_to_solve(l, array):
+def find_position_to_solve(position_to_fill, sudoku):
     for i in range(9):
         for j in range(9):
-            if array[i][j] == 0:
-                l[0] = i
-                l[1] = j
+            if sudoku[i][j] == 0:
+                position_to_fill[0] = i
+                position_to_fill[1] = j
                 return True
     return False
 
 
-def check(num, row, col, array):
+def is_location_valid(num, row, col, sudoku):
 
     num_in_row = False
     num_in_column = False
     num_in_box = False
 
     for i in range(9):
-        if array[row][i] == num:
+        if sudoku[row][i] == num:
             num_in_row = True
 
     for i in range(9):
-        if array[i][col] == num:
+        if sudoku[i][col] == num:
             num_in_column = True
 
     box_row = row - row % 3
     box_col = col - col % 3
     for i in range(3):
         for j in range(3):
-            if array[i + box_row][j + box_col] == num:
+            if sudoku[i + box_row][j + box_col] == num:
                 num_in_box = True
 
     if (not num_in_row and not num_in_column and not num_in_box):
-        array[row][col] = num
+        sudoku[row][col] = num
         return True
     return False
 
 
-def main_cycle(array):
-    l = [0, 0]
+def solve_sudoku(sudoku):
+    position_to_fill = [0, 0]
 
-    if not find_position_to_solve(l, array):
+    if not find_position_to_solve(position_to_fill, sudoku):
         print('All cells filled, exiting...')
         return True
 
-    row = l[0]
-    col = l[1]
+    row = position_to_fill[0]
+    col = position_to_fill[1]
 
     for i in range(1, 10):
-        if check(i, row, col, array):
-            if main_cycle(array):
+        if is_location_valid(i, row, col, sudoku):
+            if solve_sudoku(sudoku):
                 return True
-            array[row][col] = 0
+            sudoku[row][col] = 0
 
     return False
 
-def run(array):
+def run(sudoku):
     print('Sudoku Board - initial state')
-    print_sudoku(array)
-    main_cycle(array)
+    print_sudoku(sudoku)
+    solve_sudoku(sudoku)
     print('Sudoku Board - Final state')
-    print_sudoku(array)
-    return array
+    print_sudoku(sudoku)
+    return sudoku
 
 
 if __name__ == "__main__":
-    array = [[0 for j in range(9)] for i in range(9)]
-    run(array)
+    sudoku = [[0 for j in range(9)] for i in range(9)]
+    run(sudoku)
